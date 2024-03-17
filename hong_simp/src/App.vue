b64ed754d3b98314263576feff1efe20cfb70266
simpson <450054@naver.com>
2024-03-17 19:06:20 +0900

    [2024.03.17 simpson] 화면 구성

--- hong_simp/src/App.vue
+++ hong_simp/src/App.vue
@@ -1,26 +1,25 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <MainHeader/>
+  <MainContent/>
+  <MainFooter/>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import MainHeader from './components/MainHeader.vue'
+import MainContent from './components/MainContent.vue'
+import MainFooter from './components/MainFooter.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    MainHeader, MainContent, MainFooter
   }
 }
 </script>
 
 <style>
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
 }
 </style>
---
+++ hong_simp/src/components/MainFooter.vue
@@ -0,0 +1,16 @@
+<template>
+  <div class="MainFooter">
+    @hong_simp [2024.03.17]
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'MainFooter',
+  props: {
+  }
+}
+</script>
+
+<style scoped>
+</style>
